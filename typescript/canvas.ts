d6898d88ce2f5b312f0fe28b81416d1b29683676
Doriel Shacham <dorielshacham@gmail.com>
2023-07-23 10:20:51 +0300

    ...obj

--- typescript/canvas.ts
+++ typescript/canvas.ts
@@ -5,6 +5,7 @@ const ctx = canvas.getContext("2d")!;
 const player = new Player();
 let floors: Floor[] = [];
 
+//generate the floors
 function generateFloor() {
   const minGap = 100;
   const maxGap = 300;
@@ -12,12 +13,11 @@ function generateFloor() {
   const maxWidth = 200;
 
   const lastFloor = floors[floors.length - 1];
-  const y = lastFloor ? lastFloor.y - 100 : 350;
+  const y = lastFloor ? lastFloor.y - 100 : canvas.height - 20; // Ensure the first floor starts at the bottom of the canvas
 
   const gap = Math.floor(Math.random() * (maxGap - minGap + 1)) + minGap;
-  const width =
-    Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth;
-  const x = Math.floor(Math.random() * (500 - width));
+  const width = Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth;
+  const x = Math.floor(Math.random() * (canvas.width - width)); // Ensure the floor is within the canvas width
 
   floors.push(new Floor(x, y, width));
 }
@@ -25,48 +25,102 @@ function generateFloor() {
 function removeFloors() {
   floors = floors.filter((floor) => floor.y + floor.height > 0);
 }
+//--
 
+
+//-- gameover popup
+let gameOver = false;
+function showGameOverPopup() {
+  const popup = document.getElementById("popup")!;
+  const restartButton = document.createElement("button");
+  restartButton.textContent = "Restart";
+  restartButton.addEventListener("click", () => {
+    location.reload(); // Reload the page to restart the game
+  });
+  popup.innerHTML = "You touched the floor! Game Over!";
+  popup.appendChild(restartButton);
+  popup.style.display = "block";
+}
+//--
+
+
+//update frames
 function update() {
-  player.update();
-
-  for (const floor of floors) {
-    if (
-      player.x < floor.x + floor.width &&
-      player.x + player.width > floor.x &&
-      player.y + player.height > floor.y
-    ) {
-      player.y = floor.y - player.height;
-      player.velocityY = 0;
-      player.isJumping = false;
-      break;
+  if (!gameOver) {
+    if (isLeftKeyPressed) {
+      player.x -= 5;
+    } else if (isRightKeyPressed) {
+      player.x += 5;
     }
-  }
+    player.update();
 
-  removeFloors();
+    let floorCollision = false;
 
-  if (floors.length === 0 || floors[floors.length - 1].y > 100) {
-    generateFloor();
+    for (const floor of floors) {
+      if (
+        player.x < floor.x + floor.width &&
+        player.x + player.width > floor.x &&
+        player.y + player.height > floor.y
+      ) {
+        floorCollision = true;
+
+        if (player.isJumping) {
+          player.y = floor.y - player.height;
+          player.velocityY = 0;
+          player.isJumping = false;
+        } else {
+          // Adjust player's y velocity to "bounce" off the floor
+          player.velocityY = -10;
+        }
+        break;
+      }
+    }
+
+    // If there was no floor collision, set isJumping to true
+    if (!floorCollision) {
+      player.isJumping = true;
+    }
+
+    removeFloors();
+
+    if (floors.length === 0 || floors[floors.length - 1].y > 100) {
+      generateFloor();
+    }
+
+    if (player.y >= canvas.height) {
+      gameOver = true;
+      showGameOverPopup();
+    }
   }
 }
 
 
+
+
+//movement left right
+let isLeftKeyPressed = false;
+let isRightKeyPressed = false;
 function onKeyDown(event: KeyboardEvent) {
-    if (event.key === "ArrowLeft") {
-      player.x -= 5;
-    } else if (event.key === "ArrowRight") {
-      player.x += 5;
-    }
+  if (event.key === "ArrowLeft" || event.key === "a") {
+    isLeftKeyPressed = true;
+  } else if (event.key === "ArrowRight" || event.key === "d") {
+    isRightKeyPressed = true;
   }
-  
-  function onKeyUp(event: KeyboardEvent) {
-    // Add any additional handling if needed
+}
+function onKeyUp(event: KeyboardEvent) {
+  if (event.key === "ArrowLeft" || event.key === "a") {
+    isLeftKeyPressed = false;
+  } else if (event.key === "ArrowRight" || event.key === "d") {
+    isRightKeyPressed = false;
   }
-  
-  function onKeyPress(event: KeyboardEvent) {
-    if (event.key === " ") {
-      player.jump();
-    }
+}
+
+//jump
+function onKeyPress(event: KeyboardEvent) {
+  if (event.key === " ") {
+    player.jump();
   }
+}
   
   document.addEventListener("keydown", onKeyDown);
   document.addEventListener("keyup", onKeyUp);
@@ -81,11 +135,6 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
-canvas.addEventListener("click", () => {
-  player.jump();
-});
-
 generateFloor();
-
 setInterval(update, 1000 / 60);
 draw();
--- typescript/dist/canvas.js
+++ typescript/dist/canvas.js
@@ -3,50 +3,100 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var player = new Player();
 var floors = [];
+//generate the floors
 function generateFloor() {
     var minGap = 100;
     var maxGap = 300;
     var minWidth = 50;
     var maxWidth = 200;
     var lastFloor = floors[floors.length - 1];
-    var y = lastFloor ? lastFloor.y - 100 : 350;
+    var y = lastFloor ? lastFloor.y - 100 : canvas.height - 20; // Ensure the first floor starts at the bottom of the canvas
     var gap = Math.floor(Math.random() * (maxGap - minGap + 1)) + minGap;
     var width = Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth;
-    var x = Math.floor(Math.random() * (500 - width));
+    var x = Math.floor(Math.random() * (canvas.width - width)); // Ensure the floor is within the canvas width
     floors.push(new Floor(x, y, width));
 }
 function removeFloors() {
     floors = floors.filter(function (floor) { return floor.y + floor.height > 0; });
 }
+//--
+//-- gameover popup
+var gameOver = false;
+function showGameOverPopup() {
+    var popup = document.getElementById("popup");
+    var restartButton = document.createElement("button");
+    restartButton.textContent = "Restart";
+    restartButton.addEventListener("click", function () {
+        location.reload(); // Reload the page to restart the game
+    });
+    popup.innerHTML = "You touched the floor! Game Over!";
+    popup.appendChild(restartButton);
+    popup.style.display = "block";
+}
+//--
+//update frames
 function update() {
-    player.update();
-    for (var _i = 0, floors_1 = floors; _i < floors_1.length; _i++) {
-        var floor = floors_1[_i];
-        if (player.x < floor.x + floor.width &&
-            player.x + player.width > floor.x &&
-            player.y + player.height > floor.y) {
-            player.y = floor.y - player.height;
-            player.velocityY = 0;
-            player.isJumping = false;
-            break;
+    if (!gameOver) {
+        if (isLeftKeyPressed) {
+            player.x -= 5;
+        }
+        else if (isRightKeyPressed) {
+            player.x += 5;
+        }
+        player.update();
+        var floorCollision = false;
+        for (var _i = 0, floors_1 = floors; _i < floors_1.length; _i++) {
+            var floor = floors_1[_i];
+            if (player.x < floor.x + floor.width &&
+                player.x + player.width > floor.x &&
+                player.y + player.height > floor.y) {
+                floorCollision = true;
+                if (player.isJumping) {
+                    player.y = floor.y - player.height;
+                    player.velocityY = 0;
+                    player.isJumping = false;
+                }
+                else {
+                    // Adjust player's y velocity to "bounce" off the floor
+                    player.velocityY = -10;
+                }
+                break;
+            }
+        }
+        // If there was no floor collision, set isJumping to true
+        if (!floorCollision) {
+            player.isJumping = true;
+        }
+        removeFloors();
+        if (floors.length === 0 || floors[floors.length - 1].y > 100) {
+            generateFloor();
+        }
+        if (player.y >= canvas.height) {
+            gameOver = true;
+            showGameOverPopup();
         }
     }
-    removeFloors();
-    if (floors.length === 0 || floors[floors.length - 1].y > 100) {
-        generateFloor();
-    }
 }
+//movement left right
+var isLeftKeyPressed = false;
+var isRightKeyPressed = false;
 function onKeyDown(event) {
-    if (event.key === "ArrowLeft") {
-        player.x -= 5;
+    if (event.key === "ArrowLeft" || event.key === "a") {
+        isLeftKeyPressed = true;
     }
-    else if (event.key === "ArrowRight") {
-        player.x += 5;
+    else if (event.key === "ArrowRight" || event.key === "d") {
+        isRightKeyPressed = true;
     }
 }
 function onKeyUp(event) {
-    // Add any additional handling if needed
+    if (event.key === "ArrowLeft" || event.key === "a") {
+        isLeftKeyPressed = false;
+    }
+    else if (event.key === "ArrowRight" || event.key === "d") {
+        isRightKeyPressed = false;
+    }
 }
+//jump
 function onKeyPress(event) {
     if (event.key === " ") {
         player.jump();
@@ -65,9 +115,6 @@ function draw() {
     }
     requestAnimationFrame(draw);
 }
-canvas.addEventListener("click", function () {
-    player.jump();
-});
 generateFloor();
 setInterval(update, 1000 / 60);
 draw();
